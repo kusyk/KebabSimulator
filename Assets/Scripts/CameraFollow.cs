8a566ca0edc0d9f35e8daf1cfeec3de01cacd61b
kusyk <lukaszk133@gmail.com>
2019-05-16 18:38:50 +0200

    simple movement system

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public PlayerMovement player;
+    [SerializeField] private Vector3 offset = new Vector3(-5, 10, -5);
+    
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public Transform mainCamera;
+
+    public Transform view;
+    public Transform nearbyObjContr;
+
+    [Range(0.1f, 5)]
+    public float walkSpeed = 1.5f;
+    [Range(0.1f, 1)]
+    public float characterRotationSpeed = 0.5f;
+
+
+    private float m_angle;
+    Vector3 m_targetAngle;
+    Vector3 m_currentAngle;
+
+
+    private NavMeshAgent m_Agent;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private bool lockRotation = false;
+
+    private void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            transform.eulerAngles = new Vector3(0f, mainCamera.transform.rotation.eulerAngles.y, 0f);
+
+            Vector3 movement = new Vector3(h, 0, v);
+            if (movement.magnitude > 1)
+                movement = Vector3.Normalize(movement);
+
+            gameObject.transform.Translate(movement * walkSpeed * 0.2f);
+
+            if (lockRotation)
+            {
+                m_targetAngle = new Vector3(0f, nearbyObjContr.rotation.eulerAngles.y, 0f);
+            }
+            else
+            {
+                m_targetAngle = new Vector3(0f, m_angle, 0f);
+            }
+
+            m_angle = mainCamera.transform.rotation.eulerAngles.y + (-(Mathf.Atan2(v, h) * Mathf.Rad2Deg) + 90);
+
+            m_currentAngle = new Vector3(
+                Mathf.LerpAngle(m_currentAngle.x, m_targetAngle.x, characterRotationSpeed),
+                Mathf.LerpAngle(m_currentAngle.y, m_targetAngle.y, characterRotationSpeed),
+                Mathf.LerpAngle(m_currentAngle.z, m_targetAngle.z, characterRotationSpeed));
+
+            view.eulerAngles = m_currentAngle;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "tool")
+        {
+            lockRotation = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "tool")
+        {
+            lockRotation = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "tool")
+        {
+            nearbyObjContr.LookAt(other.transform);
+        }
+    }
+}
