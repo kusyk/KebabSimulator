a0b6f507275cbabb5d4ee2f014a09d9a05d664b0
kusyk <lukaszk133@gmail.com>
2019-05-20 23:00:43 +0200

    simple queue system

---
+++ Assets/Scripts/QueueController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class QueueController : MonoBehaviour {
+
+    private int clientsCount = 0;
+    private List<GameObject> visibleClients = new List<GameObject>();
+
+    [SerializeField] private GameObject clientPrefab;
+    [SerializeField] private Transform clientSpawn;
+    [SerializeField] private Transform[] queuePlaces;
+    
+
+    void Start () {
+        StartCoroutine(AddNewClient());
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject temp = visibleClients[0];
+            visibleClients.RemoveAt(0);
+            Destroy(temp);
+        }
+    }
+
+    private void CreateNewClient()
+    {
+        clientsCount++;
+        Debug.Log("VC.Count: " + visibleClients.Count);
+        Debug.Log("QP.Length: " + queuePlaces.Length);
+        if (visibleClients.Count < queuePlaces.Length)
+            visibleClients.Add(Instantiate(clientPrefab, clientSpawn.transform.position, Quaternion.identity) as GameObject);
+
+        SetClientsPositions(true);
+    }
+
+    private void SetClientsPositions(bool allClients)
+    {
+        for (int i = 0; i < visibleClients.Count; i++)
+        {
+            SetClientsPositions(i);
+        }
+    }
+
+    private void SetClientsPositions(int clientNo)
+    {
+        visibleClients[clientNo].GetComponent<NavMeshAgent>().SetDestination(queuePlaces[clientNo].position);
+    }
+
+    IEnumerator AddNewClient()
+    {
+        while (true)
+        {
+            CreateNewClient();
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
---
+++ GlobalSuppressions2.cs
@@ -0,0 +1,18 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:QueueController.clientsCount")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:QueueController.queuePlaces")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:CameraFollow.lockX")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:CameraFollow.lockY")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:CameraFollow.lockZ")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:Prototype.hOffset")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:QueueController.clientPrefab")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:SimpleObjectsActivator.action")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:SimpleObjectsActivator.objects")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:QueueController.clientSpawn")]
+
